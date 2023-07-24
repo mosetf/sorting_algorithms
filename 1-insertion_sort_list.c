ef18d7e6ffc8304fd504cae56bf34956712c3db2
root <root@stan.localdomain>
2023-07-24 13:31:41 +0300

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,17 +7,17 @@
  */
 void swapnodes(listint_t **h, listint_t **node1, listint_t *node2)
 {
-  (*node1)->next = node2->next;
-  if (node2->next != NULL)
-    node2->next->prev = *node1;
-  node2->prev = (*node1)->prev;
-  node2->next = *node1;
-  if ((*node1)->prev != NULL)
-    (*node1)->prev->next = node2;
-  else
-    *h = node2;
-  (*node1)->prev = node2;
-  *node1 = node2->prev;
+(*node1)->next = node2->next;
+if (node2->next != NULL)
+	node2->next->prev = *node1;
+node2->prev = (*node1)->prev;
+node2->next = *node1;
+if ((*node1)->prev != NULL)
+	(*node1)->prev->next = node2;
+else
+	*h = node2;
+(*node1)->prev = node2;
+*node1 = node2->prev;
 }
 
 /**
